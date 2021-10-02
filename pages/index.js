3344eecc67d2d04daad7a69fbff62e60885f177e
paulius <paulius@2simple.com>
2021-10-02 16:47:15 +0100

    add styles and links to post list in home page, remove some debugging code

--- pages/index.js
+++ pages/index.js
@@ -3,31 +3,16 @@ import Layout, {siteTitle} from '../components/layout'
 import utilStyles from '../styles/utils.module.scss'
 import Link from "next/link";
 import {getSortedPostsData} from "../lib/posts";
-import {useState} from "react";
+import Date from "../components/date";
 
 export default function Home({allPostsData}) {
-    const [text, setText] = useState('');
-    const [text2, setText2] = useState('');
-
-    const changeText = (event) => {
-        setText(event.target.value);
-    }
-
-    const changeText2 = (event) => {
-        setText2(event.target.value);
-    }
-
     return (
         <Layout home>
             <Head>
                 <title>{siteTitle}</title>
             </Head>
 
-            <input type="text" onChange={changeText} value={text}/>
-            <input type="text" onChange={changeText2} value={text2}/>
 
-            Output:: {text}
-            output2:: {text2}
             <section className={utilStyles.headingMd}>
                 <p>Hi my name is Paulius Gaida. I am software engineer. Mainly focusing on web development. Back-end
                     technologies node.js, PHP.
@@ -39,10 +24,6 @@ export default function Home({allPostsData}) {
                     <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
                 </p>
             </section>
-            <Link href="/posts/first-post">
-                <a>First Post</a>
-            </Link>
-
 
             {/* Add this <section> tag below the existing <section> tag */}
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
@@ -50,11 +31,13 @@ export default function Home({allPostsData}) {
                 <ul className={utilStyles.list}>
                     {allPostsData.map(({id, date, title}) => (
                         <li className={utilStyles.listItem} key={id}>
-                            {title}
+                            <Link href={`posts/${id}`}>
+                                <a>{title}</a>
+                            </Link>
                             <br/>
-                            {id}
-                            <br/>
-                            {date}
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date} />
+                            </small>
                         </li>
                     ))}
                 </ul>
